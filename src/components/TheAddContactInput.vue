ed653b55599196d4e89d7a5a39f36f6668f09d8a
Braden Wong <bmw02002turbo@gmail.com>
2022-07-30 00:26:52 +0100

    Feat: Options List

--- src/components/TheAddContactInput.vue
+++ src/components/TheAddContactInput.vue
@@ -7,6 +7,9 @@
     fill-input
     input-debounce="0"
     :options="options"
+    option-value="email"
+    option-label="full_name"
+    option-disable="inactive"
     @filter="filterFn"
     @input-value="setModel"
     hint="Text autocomplete"
@@ -23,6 +26,7 @@
 import { supabase } from '../supabase';
 import { definitions } from 'components/supabase';
 import { ref } from 'vue';
+import ContactsList from 'src/components/ContactsList.vue';
 
 // Select all contacts from "facebook" table whose "email", "first_name", or "last_name" is not null
 const { data: allContacts, error } = await supabase
@@ -32,13 +36,23 @@ const { data: allContacts, error } = await supabase
   .not('first_name', 'is', null)
   .not('last_name', 'is', null);
 console.log(allContacts);
+// allContacts?.forEach((contact) => {
+//   contact.full_name = `${contact.first_name} ${contact.last_name}`;
+// });
+const allOptions = allContacts?.map(
+  ({ email, first_name, last_name, image }) => ({
+    email,
+    full_name: `${first_name} ${last_name}`,
+    image,
+  })
+);
 
 const model = ref(null);
-const options = ref(allContacts);
+const options = ref(allOptions);
 function filterFn(val, update, abort) {
   update(() => {
     const needle = val.toLocaleLowerCase();
-    options.value = allContacts.filter(
+    options.value = allOptions.filter(
       ({ email, first_name, last_name }) =>
         email.toLocaleLowerCase().indexOf(needle) > -1
     );
