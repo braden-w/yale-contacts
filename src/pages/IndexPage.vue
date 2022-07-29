b4a60bf1bdb714df0c50bc7e1ee8329215d27d38
Braden Wong <bmw02002turbo@gmail.com>
2022-07-29 22:22:44 +0100

    Feat: Fetch all Contacts from Braden

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -7,7 +7,18 @@ import { supabase } from '../supabase';
 import { definitions } from 'components/supabase';
 import { ref } from 'vue';
 
-// Import all contacts that are connected to "braden.wong@yale.edu"
+// Import all contacts that are connected to "braden.wong@yale.edu" from relationships_between_users
+const fetchContacts = async () => {
+  const { data, error } = await supabase
+    .from('relationships_between_users')
+    .select('*')
+    .eq('from_email', 'braden.wong@yale.edu');
+  if (error) {
+    console.error(error);
+  }
+  return data;
+};
 
+console.log(await fetchContacts());
 const contacts = ref<definitions['users'][]>([]);
 </script>
