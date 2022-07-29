e5c3d018be974642863a37c5ae9b2a9de5490c45
Braden Wong <bmw02002turbo@gmail.com>
2022-07-29 22:15:06 +0100

    Feat: Clear Page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,12 +1,5 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <q-page class="row items-center justify-evenly"> </q-page>
 </template>
 
 <script setup lang="ts">
