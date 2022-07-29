49d1f21f94f7d0efbad95acf5260accd1e855630
Braden Wong <bmw02002turbo@gmail.com>
2022-07-29 22:19:30 +0100

    Feat: Setup Supabase

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,8 +3,11 @@
 </template>
 
 <script setup lang="ts">
+import { supabase } from '../supabase';
 import { definitions } from 'components/supabase';
 import { ref } from 'vue';
+
+// Import all contacts that are connected to "braden.wong@yale.edu"
 
 const contacts = ref<definitions['users'][]>([]);
 </script>
