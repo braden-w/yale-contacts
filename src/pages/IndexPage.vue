845c164c70204173943a67278e34a751bd437206
Braden Wong <bmw02002turbo@gmail.com>
2022-07-29 23:35:34 +0100

    Feat: Filter

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,19 @@
 <template>
   <q-page>
+    <q-card class="q-mb-md">
+      <q-input
+        v-model="filter"
+        standout
+        dense
+        debounce="300"
+        placeholder="Filter..."
+        style="width: 100%"
+      >
+        <template #append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+    </q-card>
     <Suspense timeout="0">
       <!-- Make beautiful list of contact photos, name, college -->
       <template #default> <ContactsList /></template>
@@ -10,4 +24,5 @@
 
 <script setup lang="ts">
 import ContactsList from '../components/ContactsList.vue';
+const filter = ref<string>('');
 </script>
