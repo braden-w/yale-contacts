b393361c6253bc73fd5856fa46d09917a9cff45c
Braden Wong <bmw02002turbo@gmail.com>
2022-07-30 00:40:47 +0100

    Feat: Faster Add Logic

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -78,6 +78,48 @@
 
     <q-page-container>
       <router-view />
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn fab icon="campaign" @click="openAddDialog()" color="accent">
+          Add Contacts
+        </q-btn>
+        <q-dialog v-model="addContactDialog" seamless position="bottom">
+          <q-card style="width: 100%">
+            <q-card-section>
+              <div class="row">
+                <div class="text-h6">Report an Issue</div>
+                <q-space></q-space>
+                <q-btn round flat icon="close" v-close-popup></q-btn>
+              </div>
+              <div class="text-caption">
+                We'll do our best to respond within the night! This form emails
+                us at
+                <a
+                  style="color: inherit"
+                  href="mailto:yalebutterybook@gmail.com"
+                >
+                  yalebutterybook@gmail.com
+                </a>
+              </div>
+            </q-card-section>
+            <q-separator />
+            <q-form @submit="() => {}">
+              <q-card-section> Input Goes Here </q-card-section>
+              <q-card-actions>
+                <q-space></q-space>
+                <q-btn label="Close" padding="xs lg" flat v-close-popup>
+                </q-btn>
+                <q-btn
+                  label="Submit"
+                  color="accent"
+                  padding="xs lg"
+                  type="submit"
+                >
+                </q-btn>
+              </q-card-actions>
+            </q-form>
+          </q-card>
+        </q-dialog>
+      </q-page-sticky>
     </q-page-container>
   </q-layout>
 </template>
@@ -134,6 +176,11 @@ const essentialLinks: EssentialLinkProps[] = [
 ];
 
 const leftDrawerOpen = ref(false);
+
+const addContactDialog = ref(false);
+function openAddDialog() {
+  addContactDialog.value = true;
+}
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
