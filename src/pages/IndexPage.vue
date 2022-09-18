159477447c62bd9675b0c158aeda97d44ecb7595
Braden Wong <bmw02002turbo@gmail.com>
2022-09-17 22:53:44 -0400

    Feat: Loading

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -37,8 +37,31 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { supabase } from '../supabase';
+import { definitions } from 'app/types/supabase';
 import ContactsList from '../components/ContactsList.vue';
 import TheAddContactInput from '../components/TheAddContactInput.vue';
+
 const filter = ref('');
 const tab = ref('best');
+
+// Import all contacts that are connected to "braden.wong@yale.edu" from relationships_between_users
+const contacts = ref<definitions['facebook'][] | null>([]);
+
+const fetchContacts = async (): Promise<definitions['facebook'][] | null> => {
+  // Select all contacts from "users" table whose "email" is
+  const { data, error } = await supabase
+    .from('relationships_between_users')
+    .select('facebook(*)')
+    .eq('from_email', 'braden.wong@yale.edu');
+  if (error) {
+    console.error(error);
+  }
+  // Set contacts to the result of fetchContacts
+  if (!data) return null;
+  const facebook = data.map(({ facebook }) => facebook);
+  contacts.value = facebook;
+  return facebook;
+};
+fetchContacts();
 </script>
